a2567a143276cc4742d64c8158d176d7d293ba70
victorsaba126 <victorsabariego_bcnvj1ma1920@cev.com>
2021-05-12 12:01:27 +0200

    CreditTimer

---
+++ Assets/scripts/CreditTimer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CreditTimer : MonoBehaviour
+{
+    [SerializeField]
+    private float delayBeforeLoading = 28f;
+
+    [SerializeField]
+    private string sceneNameToLoad;
+
+    private float timeElapsed;
+    private void Update()
+    {
+        timeElapsed += Time.deltaTime;
+
+        if(timeElapsed > delayBeforeLoading)
+        {
+            SceneManager.LoadScene(sceneNameToLoad);
+        }
+    }
+}
