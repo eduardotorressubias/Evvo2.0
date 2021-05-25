6232c97105561044fa70847ed5c6d47165a77214
DaniCollado <72010031+DaniCollado@users.noreply.github.com>
2021-05-26 00:58:58 +0200

    main_menu_mejorado

---
+++ Assets/scripts/SonidoBotones.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SonidoBotones : MonoBehaviour
+{
+    public AudioSource click;
+    public AudioClip clip;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        click.clip = clip;
+    }
+
+    public void Reproducir ()
+    {
+        click.Play();
+    }
+}
