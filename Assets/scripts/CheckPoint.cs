8de43e59f01cb37c1744c21d2896f7dc5f95aef1
eduardotorressubias <edu.torres9@hotmail.com>
2021-06-04 01:08:47 +0200

    progreso_checkpoint

---
+++ Assets/scripts/CheckPoint.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckPoint : MonoBehaviour
+{
+    static public Transform [] checkpoint;
+    public static int check = 0;
+    void Awake()
+    {
+        for (int i = 0; i < checkpoint.Length; i++)
+        {
+            
+            if (PlayerPrefs.GetInt("CheckPoint") == i)
+            {
+                transform.position = checkpoint[i].position;
+            }
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "check")
+        {
+            Debug.Log("hola");
+            PlayerPrefs.SetInt("CheckPoint", check);
+            check++;
+        }
+
+    }
+}
